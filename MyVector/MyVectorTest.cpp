fd3fef36a9a026422d9b4cc5305fb83229c65a91
JarrettW <JarrettW@github.com>
2016-03-11 22:07:31 +0800

    update

--- MyVector/MyVectorTest.cpp
+++ MyVector/MyVectorTest.cpp
@@ -4,10 +4,11 @@
 
 int main(){
     MyVector<int> DefaultConstructor; //默认构造函数
-    MyVector<int> (10);
-    for(auto i = 0; i != DefaultConstructor.size(); ++i)
-        std::cout << DefaultConstructor[i] << " ";
-    
+    int a = 10;
+    MyVector<int> V1(a);  //初始化为n个0
+    for(auto i = 0; i != V1.size(); ++i)
+        std::cout << V1[i] << " ";
+       
 	std::cout << std::endl;
     std::cout << "Time consuming: " << clock();
 	return 0;
