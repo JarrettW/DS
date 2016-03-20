6051da2bf0a5fc4960245446526e3ad459142823
JarrettW <JarrettW@github.com>
2016-03-20 21:51:42 +0800

    update

--- MyBinTree/MyBinTree.h
+++ MyBinTree/MyBinTree.h
@@ -12,6 +12,32 @@ using BinNodePosi(T) = BinNode<T>*;
 //节点颜色,红黑树使用
 typedef enum{ RB_RED, RB_BLACK } RBColor;
 
+/************************************以宏的形式对基于BinNode的操作作一归纳整理*************************************/
+//是否为根节点
+#define IsRoot(x) (!(x.parent)) //根节点的父亲为NULL
+//是否为左孩子
+#define IsLChild(x) ( !IsRoot(x) && (& (x) == (x).parent->lc ))
+//是否为右孩子
+#define IsRChild(x) ( !IsRoot(x) && (& (x) == (x).parent->rc ))
+//是否有父节点
+#define HasParent(x) ( !IsRoot(x) ) //不为根节点,就有父节点
+//是否有左孩子
+#define HasLChild(x) ( (x).lc )
+//是否有右孩子
+#define HasRChild(x) ( (x.)rc )
+//至少有一个孩子
+#define HasOneChild(x) ( HasLChild(x) || HasRChild(x) )
+//同时拥有两个孩子
+#define HasBothChild(x) ( HasRChild(x) && HasLChild(x) )
+//是否为叶节点
+#define IsLeaf(x) ( !HasOneChild(x) )
+//兄弟关系的节点和指针
+#define sibling(p) ( IsLChild( *(p) ) ? (p)->parent->rc : (p)->parent-lc )
+//叔叔关系的节点和指针
+#define uncle(p) ( IsLChild( *( (p)->parent ) ) ? (p)->parent->parent->rc : (p)->parent->parent->lc )
+//来自父亲的引用
+#define FromParentTo(x) ( IsRoot(x) ? _root : (IsLChild(x) ? (x).parent->lc : (x).parent->rc ))
+/***************************************************************************************************************/
 //二叉树节点模板类
 template <typename T>
 class BinNode{
@@ -78,49 +104,59 @@ public:
     //后序遍历统一接口,随机选取递归版或迭代版
     template <typename VST>
     void travPost(VST &visit);
-    
-    
 };
 
 //二叉树模板类
 template <typename T>
-class MyBinTree: public MyList<T>{
+class MyBinTree:{
 public:
     //默认构造函数
-    MyBinTree() { _root = NULL; }
-    //将节点作为左孩子插入
+    MyBinTree():_size(0), _root(NULL) {}
+    //析构函数
     
-    //将节点作为右孩子插入
+    //返回规模
     
-    //删除当前节点的左孩子
+    //判空
     
-    //删除当前节点的右孩子
+    //返回根节点
     
-    //将一棵树作为左孩子接入
+    //插入根节点
+        
+    //将元素e作为节点x的左孩子(原无)插入
     
-    //将一棵数作为右孩子接入
+    //将元素e作为节点x的右孩子(原无)插入
     
-    //摘除左子树,并取出
+    //T作为左子树接入
     
-    //摘除右子树,并取出
+    //T作为右子树接入
     
-    //函数对象
-    //先序遍历---递归版
+    //删除以位置x处节点为根的子树, 返回该子树原先的规模
     
-    //中序遍历---递归版
+    //递归删除左右子树
     
-    //后序遍历---递归版
+    //将子树x从当前树中摘除,并将其转换为一棵独立子树
     
-    //先序遍历---迭代版
+    //函数对象, 遍历
+    //先序遍历
     
-    //中序遍历---迭代版
+    //中序遍历
     
-    //后序遍历---迭代版
+    //后序遍历
     
-private:
+    //层次遍历
+    
+    //比较操作符
+    
+protected:
+    //规模
+    int _size;
     //根节点
     BinNodePosi(T) _root;
-    //规模
-    unsigned int _size;
+    //更新节点的高度
+    
+    //更新节点高度及其祖先的高度
+    
+    //在两个元素中选取较大者
+    int max(const T, const t);
 };
 #endif
