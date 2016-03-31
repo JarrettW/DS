d10f9f831140a931d9f0c73d8559c31556671945
JarrettW <JarrettW@github.com>
2016-03-31 22:19:15 +0800

    Done! MyLeftHeap

--- MyPQ/MyLeftHeapTest.cpp
+++ MyPQ/MyLeftHeapTest.cpp
@@ -11,14 +11,14 @@ int main(){
     int arr[6] { 23, 45, 12, 53, 54, 12 };
     MyLeftHeap<int> MLH(arr, 6);
     MLH.insert(999);
-    std::cout << "MLH: \n";
+    /*std::cout << "MLH: \n";
     MLH.travIn( Visit<int>() );
     std::cout << std::endl;
     std::cout << "getMax: " << MLH.getMax() << std::endl;
     std::cout << "delMax: " << MLH.delMax() << std::endl;
     
     std::cout << "MLH: \n";
-    MLH.travIn( Visit<int>() );
+    MLH.travIn( Visit<int>() );*/
     
     return 0;
 }
